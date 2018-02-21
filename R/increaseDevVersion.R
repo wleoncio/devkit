343896f4a1edb473bee450f2435b42d9d434b515
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-02-21 09:58:30 +0100

    Added increaseDevVersion().

---
+++ R/increaseDevVersion.R
@@ -0,0 +1,33 @@
+#' Increase package version in DESCRIPTION
+#' @param sep character separating major.minor.patch version from build
+#'   identifier
+#' @importFrom utils packageVersion
+#' @export
+#'
+increaseDevVersion <- function(sep = "-") {
+  # Extract
+  description <- readLines("DESCRIPTION")
+  for (line in 1:length(description)) {
+    if (substr(description[line], 1, 7) == "Package") pkg <- description[line]
+    if (substr(description[line], 1, 7) == "Version") {
+      str <- description[line]
+      break
+    }
+  }
+  cat(gsub("Package: " , "", pkg), "updated from", str)
+  regSep      <- paste0("\\", sep)
+  str.split   <- strsplit(str, regSep)[[1]]
+  dev.version <- as.numeric(str.split[length(str.split)])
+
+  # Update
+  dev.version.updated <- dev.version + 1
+
+  # Reassemble
+  fixed.str   <- str.split[1:(length(str.split) - 1)]
+  str.updated <- paste(c(fixed.str, dev.version.updated), collapse = sep)
+  description[line] <- str.updated
+  cat(" to", str.updated)
+
+  # Output
+  writeLines(description, "DESCRIPTION")
+}
